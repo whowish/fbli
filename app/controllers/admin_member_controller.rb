ebfffcb99f72465c31ca4a3909c84eb1b91e9a9a
Nilobol <moopino@hotmail.com>
2012-05-17 11:35:17 +0700

    set admin

--- app/controllers/admin_member_controller.rb
+++ app/controllers/admin_member_controller.rb
@@ -13,4 +13,32 @@ class AdminMemberController < ApplicationController
     end
     render :json=>{:ok=>true}
   end
+  
+  def set_admin
+    @members = Member.where(:is_admin=>false).entries
+  end
+  
+  def submit_set_admin
+    member_ids = params[:member_id].split(',');
+    member_ids.each do |id|
+      member = FeaturedMember.first(:conditions=>{:_id => id})
+      member.is_admin = true
+      member.save
+    end
+    render :json=>{:ok=>true}
+  end
+  
+  def remove_admin
+    @admins = Member.where(:is_admin=>true).entries
+  end
+  
+  def submit_remove_admin
+    member_ids = params[:member_id].split(',');
+    member_ids.each do |id|
+      member = FeaturedMember.first(:conditions=>{:_id => id})
+      member.is_admin = false
+      member.save
+    end
+    render :json=>{:ok=>true}
+  end
 end
